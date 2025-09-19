29d2503bf239ff28342f21d44e8ec9fba834e69b
florentianayuwono <florentiana.yuwono@canonical.com>
2025-09-19 13:28:46 +0700

    move conftests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,29 @@
 # See LICENSE file for licensing details.
 
 """Fixtures for charm tests."""
+
+
+def pytest_addoption(parser):
+    """Add custom command-line options to pytest.
+
+    Args:
+        parser: The pytest command-line parser.
+    """
+    parser.addoption(
+        "--charm-file",
+        action="append",
+        default=[],
+        help="Path(s) to built charm file(s) to use in tests",
+    )
+    parser.addoption(
+        "--model",
+        action="store",
+        default=None,
+        help="Use an existing Juju model instead of creating a temporary one",
+    )
+    parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="Keep Juju models around after tests instead of destroying them",
+    )
