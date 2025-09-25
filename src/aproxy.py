cf87edd77821cf15c1419b077f73a99dc6d3bb14
florentianayuwono <florentiana.yuwono@canonical.com>
2025-09-25 21:42:19 +0700

    modify ip list

--- src/aproxy.py
+++ src/aproxy.py
@@ -198,9 +198,6 @@ class AproxyManager:
         ports_clause = ", ".join(self.config.intercept_ports_list)
         excluded_ips = [
             "127.0.0.0/8",
-            "10.0.0.0/8",
-            "172.16.0.0/12",
-            "192.168.0.0/16",
         ] + self._get_no_proxy_ips()
 
         return f"""#!/usr/sbin/nft -f
