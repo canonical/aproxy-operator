54682c0f4735efc963f3055e0bc2aa2f003a15ab
florentianayuwono <florentiana.yuwono@canonical.com>
2025-10-08 16:48:36 +0700

    fix formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,13 @@
 # See LICENSE file for licensing details.
 
 resource "juju_application" "aproxy" {
-  name            = var.app_name
-  charm           = var.charm_name
-  channel         = var.channel
-  model           = var.model
-  base            = var.base
-  revision        = var.revision
+  name     = var.app_name
+  charm    = var.charm_name
+  channel  = var.channel
+  model    = var.model
+  base     = var.base
+  revision = var.revision
 
-  config          = var.config
-  units           = 0
+  config = var.config
+  units  = 0
 }
