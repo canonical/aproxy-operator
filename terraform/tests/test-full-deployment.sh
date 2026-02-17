baa362d40ed942ab150b0be28742e280c670b5a4
Sebastien Georget <sebastien.georget@canonical.com>
2026-02-17 14:44:33 +0100

    Fix additional tests

--- terraform/tests/test-full-deployment.sh
+++ terraform/tests/test-full-deployment.sh
@@ -12,8 +12,14 @@ set -euo pipefail
 
 echo "Running additional deployment tests..."
 
+terraform init --upgrade
+terraform apply -auto-approve
+
+juju deploy ubuntu --channel=latest/stable --model tf-testing-lxd
+
 juju integrate ubuntu aproxy --model tf-testing-lxd
 juju wait-for application aproxy --query='status=="maintenance" || status=="blocked"' --timeout=10m
+STATUS=$(juju status aproxy --model test-aproxy-example --format=json | jq -r '.applications.aproxy["application-status"].current')
 echo "aproxy status: $STATUS"
 if [ "$STATUS" == "error" ] || [ "$STATUS" == "unknown" ]; then
   echo "aproxy failed to deploy or is unknown"
